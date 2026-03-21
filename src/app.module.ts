1c8230b77732d26119ff39248f816d4b4536536e
JoseMateoPosso <joseteo@hotmail.es>
2026-03-20 21:42:07 -0500

    feat: implement dynamic categories fetch from API to replace hardcoded list

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { TouristSpotsModule } from './tourist-spots/tourist-spots.module';
 import { AuthModule } from './auth/auth.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { StorageModule } from './storage/storage.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, TouristSpotsModule, AuthModule, ReviewsModule, StorageModule],
+  imports: [PrismaModule, UsersModule, TouristSpotsModule, AuthModule, ReviewsModule, StorageModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
